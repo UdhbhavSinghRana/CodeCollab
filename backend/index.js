c6d09e9c4eff36e49ffb8f221d6e56c1503cdbf1
Udhbhav <21889udbhav@gmail.com>
2024-07-06 23:12:57 +0530

    Create socket file for backend

---
+++ backend/index.js
@@ -0,0 +1,48 @@
+const express = require('express');
+const http = require('http');
+const socketIo = require('socket.io');
+
+const app = express();
+const server = http.createServer(app);
+const io = socketIo(server);
+
+io.on('connection', (socket) => {
+  console.log('New client connected');
+
+  socket.on('disconnect', () => {
+    console.log('Client disconnected');
+  });
+
+  socket.on('join-room', (room) => {
+    socket.join(room);
+  })
+
+  socket.on('code-update', (data) => {
+    io.to(data.room).emit('code-update', data);
+  })
+
+  socket.on('input-update', (data) => {
+    io.to(data.room).emit('input-update', data);
+  })
+
+  socket.on('run-code', (data) => {
+    io.to(data.room).emit('run-code', data);
+  })
+
+  socket.on('output-update', (data) => {
+    io.to(data.room).emit('output-update', data);
+  })
+
+  socket.on('theme-update', (data) => {
+    io.to(data.room).emit('theme-update', data);
+  })
+
+  socket.on('lang-update', (data) => {
+    io.to(data.room).emit('lang-update', data);
+  })
+});
+
+const PORT = process.env.PORT || 4000;
+server.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});

f963a71188ec7348e9e81854eaa86c0faab61336
Udhbhav <21889udbhav@gmail.com>
2024-07-07 00:02:54 +0530

    Add react-router-dom

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,16 +2,21 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Header from './components/Header'
 import Editor from './components/Editor'
+import Home from './Home'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
-      <Header />
-      <Editor />
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/room*" element={<Navigate to="/" />} />
+      </Routes>
+    </Router>
     </>
   )
 }

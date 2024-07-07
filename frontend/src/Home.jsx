35e860c6d8fd64b79bad5cecafced5f6f8e4d891
Udhbhav <21889udbhav@gmail.com>
2024-07-07 19:04:46 +0530

    Resolve mege conflict

--- frontend/src/Home.jsx
+++ frontend/src/Home.jsx
@@ -1,11 +1,19 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Header from './components/Header'
 import Editor from './components/Editor'
+import ProblemsPage from "./components/Problems/problems";// Import the Problems component
+
 
 function Home() {
+  const [isSideDrawerOpen, setIsSideDrawerOpen] = useState(false);
+
+  const toggleSidebar = () => {
+    setIsSideDrawerOpen(!isSideDrawerOpen);
+  };
   return (
     <div>
-        <Header />
+        <Header toggleSidebar={toggleSidebar}/>
+        <ProblemsPage  isOpen={isSideDrawerOpen} setIsSideDrawerOpen={setIsSideDrawerOpen}/>
         <Editor />
     </div>
   )

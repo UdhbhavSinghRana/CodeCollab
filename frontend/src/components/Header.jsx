896bfbf98bc10aaf25dfb8fd15552c2121adb078
Udhbhav <21889udbhav@gmail.com>
2024-07-09 02:26:16 +0530

    Create code editor functionality in header

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -15,21 +15,26 @@ function Header({toggleSidebar, toggleWhiteboard}) {
     const showNotification = (message) => {
         toast.success(message);
     };
+
+    const scrollDownn = () => {
+        window.scrollTo({
+            top: document.body.scrollHeight,
+            behavior: 'smooth'
+        });
+    }
+
     return (
         <div className='flex justify-items-center bg-gray-800'>
             <div className='w-fit text'>
                 <img src={Logo} width={300} alt="Logo" />
             </div>
             <div className='text-slate-200 flex justify-end w-full gap-16 items-center pr-10'>
-                <div className='hover:border-2 hover:bg-gray-700 p-2 rounded-md'>
-                    Record Session
-                </div>
                 <div className='hover:border-2 hover:bg-gray-700 p-2 rounded-md flex items-center'
                     onClick={copyRoomURLToClipboard}>
                     <IoCopy className='mr-1' />
                     <span>Room URL</span>
                 </div>
-                <div className='hover:border-2 hover:bg-gray-700 p-2 rounded-md'>
+                <div className='hover:border-2 hover:bg-gray-700 p-2 rounded-md' onClick={scrollDownn}>
                     Code Editor
                 </div>
                 <div className='hover:border-2 hover:bg-gray-700 hover:transition-delay: 500ms p-2 rounded-md' onClick={toggleWhiteboard}>

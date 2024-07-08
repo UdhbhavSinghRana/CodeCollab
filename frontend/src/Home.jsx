7c3ff7832e2089541b8732f54f089619b953d915
Udhbhav <21889udbhav@gmail.com>
2024-07-09 01:00:12 +0530

    Fix path direction to random room

--- frontend/src/Home.jsx
+++ frontend/src/Home.jsx
@@ -1,15 +1,27 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import Header from './components/Header'
 import Editor from './components/Editor'
 import ProblemsPage from "./components/Problems/problems";// Import the Problems component
+import { useLocation, useNavigate } from 'react-router-dom';
 
 
 function Home() {
+  const navigate = useNavigate();
+  const location = useLocation();
   const [isSideDrawerOpen, setIsSideDrawerOpen] = useState(false);
 
   const toggleSidebar = () => {
     setIsSideDrawerOpen(!isSideDrawerOpen);
   };
+
+  // Redirect to a random room number
+  useEffect(() => {
+    if (location.pathname === '/') {
+      const roomNumber = Math.floor(Math.random() * 1000); 
+      navigate(`/room?room=${roomNumber}`);
+    }
+  }, [location, navigate]);
+
   return (
     <div>
         <Header toggleSidebar={toggleSidebar}/>
